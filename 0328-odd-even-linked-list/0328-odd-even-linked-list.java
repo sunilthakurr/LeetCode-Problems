4e4576920268ed664601244d48ed3af692a1d5ef
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2022-12-06 07:30:48 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (72.15%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null)
+			return head;
+        ListNode temp1 = head;
+		ListNode temp2 = head.next;
+        ListNode evenHead = temp2;
+        
+        while(temp1.next != null && temp2.next != null){
+            temp1.next = temp2.next;
+            temp2.next = temp2.next.next;
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+        temp1.next = evenHead;
+        return head;
+    }
+}
