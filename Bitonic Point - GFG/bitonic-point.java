d4c72c3266499c1704c954e9c3e508b18fcb3fbd
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2023-06-29 18:32:34 +0530

    Added solution - LeetHub

--- Bitonic Point - GFG/bitonic-point.java
+++ Bitonic Point - GFG/bitonic-point.java
@@ -35,11 +35,14 @@ public class Main {
 class Solution {
     int findMaximum(int[] arr, int n) {
         // code here
-       int max = arr[0];
-       for(int i = 0; i <= n - 1; ++i){
-           if(arr[i] > max)
-              max = arr[i];
+       if(arr[n - 1] > arr[n - 2]){
+           return arr[n - 1];
        }
-       return max;
+       for(int i = 1; i < n; ++i){
+           if(arr[i - 1] > arr[i])
+              return arr[i - 1];
+       }
+       return -1;
     }
+    
 }
