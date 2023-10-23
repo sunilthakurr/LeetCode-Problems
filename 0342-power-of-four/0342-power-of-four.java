8f59e252be9d2ece2f5d645e3f612623e5c97d6e
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2023-10-23 22:16:46 +0530

    Time: 1 ms (76.31%), Space: 39.2 MB (83.82%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n == 1){
-            return true;
-        }
-        if(n <= 0 || n % 4 != 0){
+        if(n <= 0){
             return false;
         }
-        return isPowerOfFour(n / 4);
+        double exp = Math.log(n) / Math.log(4);
+
+        return exp == Math.floor(exp);
     }
 }
