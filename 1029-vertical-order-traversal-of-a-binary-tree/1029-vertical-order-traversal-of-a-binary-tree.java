63bd2f1d500a2f4c6aee18d81b93bd097ff1c1c3
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2025-01-23 01:11:30 +0530

    Time: 4 ms (40.48%), Space: 43.1 MB (7.43%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -14,78 +14,72 @@
  * }
  */
 class Solution {
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
-        return solve(root);
-    }
-    public List<List<Integer>> solve(TreeNode root){
+    class Pair {
+        TreeNode node;
+        int row;
+        int col;
         
-        // root -> row -> col
-        Queue<Pair<TreeNode, Pair<Integer, Integer>>> q = new LinkedList<>();
-
-        q.add(new Pair(root, new Pair(0, 0)));
-
-        // col -> row -> all elements of that particular row
-        HashMap<Integer, HashMap<Integer, List<Integer>>> map = new HashMap<>();
-
-        int min = 0;
-        int max = 0;
-
-        while(!q.isEmpty()){
-            TreeNode node = q.peek().getKey();
-            int row = q.peek().getValue().getKey();
-            int col = q.peek().getValue().getValue();
-
-            q.poll();
-
-            min = Math.min(min, col);
-            max = Math.max(max, col);
-            if(!map.containsKey(col)){
-                List<Integer> list = new ArrayList<>();
-                list.add(node.val);
-                HashMap<Integer, List<Integer>> mp = new HashMap<>();
-                mp.put(row, list);
-                map.put(col, mp);
-            }
-            else{
-                HashMap<Integer, List<Integer>> mp = map.get(col);
-                if(!mp.containsKey(row)){
+        public Pair(TreeNode node, int row, int col) {
+            this.node = node;
+            this.row = row;
+            this.col = col;
+        }
+    }
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        Queue<Pair> q = new LinkedList<>();
+        TreeMap<Integer, TreeMap<Integer, List<Integer>>> map = new TreeMap<>();
+        
+        q.add(new Pair(root, 0, 0));
+        
+        while(!q.isEmpty()) {
+            int size = q.size();
+            
+            for(int i = 0; i < size; i++) {
+                TreeNode node = q.peek().node;
+                int row = q.peek().row;
+                int col = q.peek().col;
+                q.poll();
+                
+                if(!map.containsKey(col)) {
+                    TreeMap<Integer, List<Integer>> colMap = new TreeMap<>();
                     List<Integer> list = new ArrayList<>();
                     list.add(node.val);
-                    mp.put(row, list);
+                    colMap.put(row, list);
+                    map.put(col, colMap);
                 }
-                else{
-                    List<Integer> list = mp.get(row);
-                    list.add(node.val);
-                    Collections.sort(list);
-                    mp.put(row, list);
+                else {
+                    TreeMap<Integer, List<Integer>> colMap = map.get(col);
+                    if(!colMap.containsKey(row)) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(node.val);
+                        colMap.put(row, list);
+                    }
+                    else {
+                        List<Integer> list = colMap.get(row);
+                        list.add(node.val);
+                        colMap.put(row, list);
+                    }
+                    map.put(col, colMap);
                 }
-            }
-            if(node.left != null){
-                q.add(new Pair(node.left, new Pair(row + 1, col - 1)));
-            }
-            if(node.right != null){
-                q.add(new Pair(node.right, new Pair(row + 1, col + 1)));
+                if(node.left != null) {
+                    q.add(new Pair(node.left, row + 1, col - 1));
+                }
+                if(node.right != null) {
+                    q.add(new Pair(node.right, row + 1, col + 1));
+                }
             }
         }
         List<List<Integer>> res = new ArrayList<>();
-        // System.out.println(map.deepToString());
-
-        for(int i = min; i <= max; i++){
-            HashMap<Integer, List<Integer>> temp = map.get(i);
-            List<Integer> arr = new ArrayList<>(temp.keySet());
-            Collections.sort(arr);
-            List<Integer> abc = new ArrayList<>();
-
-            for(int k = 0; k < arr.size(); k++){
-                List<Integer> bcd = temp.get(arr.get(k));
-
-                for(int j = 0; j < bcd.size(); ++j){
-                    abc.add(bcd.get(j));
-                }
+        for(Map.Entry<Integer, TreeMap<Integer, List<Integer>>> entry: map.entrySet()) {
+            TreeMap<Integer, List<Integer>> colMap = entry.getValue();
+            List<Integer> list = new ArrayList<>();
+            for(Map.Entry<Integer, List<Integer>> colEntry: colMap.entrySet()) {
+                List<Integer> rowList = colEntry.getValue();
+                Collections.sort(rowList);
+                list.addAll(rowList);
             }
-            res.add(abc);
+            res.add(list);
         }
-
         return res;
     }
 }
