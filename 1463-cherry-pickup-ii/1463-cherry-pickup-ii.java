965e16df4acd09d996a2013041d7a0023cc3c125
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2026-02-03 13:29:15 +0530

    Time: 26 ms (57.57%), Space: 46.5 MB (44.05%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,12 +1,56 @@
 class Solution {
     public int cherryPickup(int[][] grid) {
-        int[][][] container = new int[grid.length][grid[0].length][grid[0].length];
-        for (int i = 0; i < container.length; i++) {
-            for (int j = 0; j < container[0].length; j++) {
-                Arrays.fill(container[i][j], -1);
+        // int[][][] container = new int[grid.length][grid[0].length][grid[0].length];
+        // for (int i = 0; i < container.length; i++) {
+        //     for (int j = 0; j < container[0].length; j++) {
+        //         Arrays.fill(container[i][j], -1);
+        //     }
+        // }
+        // return solve(0, 0, grid[0].length - 1, grid, container);
+        return iterDP(grid);
+    }
+    private int iterDP(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int[][][] dp = new int[n][m][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                Arrays.fill(dp[i][j], -1);
             }
         }
-        return solve(0, 0, grid[0].length - 1, grid, container);
+        dp[0][0][m - 1] = grid[0][0] + grid[0][m - 1];
+        
+        int ans = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < m; k++) {
+                    int max = -1;
+
+                    for (int x = -1; x <= 1; x++) {
+                        for (int y = -1; y <= 1; y++) {
+                            if (j + x >= 0 && j + x < m && k + y >= 0 && k + y < m) {
+                                max = Math.max(max, dp[i - 1][j + x][k + y]);
+                            }
+                        }
+                    }
+                    // if (j == k) {
+                    //     dp[i][j][k] = max + grid[i][j];
+                    // } else {
+                    if (max != -1) {
+                        if (j == k) {
+                            dp[i][j][k] = max + grid[i][j];
+                        } else {
+                            dp[i][j][k] = max + grid[i][j] + grid[i][k];
+                        }
+                    }
+                    ans = Math.max(ans, dp[i][j][k]);
+                }
+            }
+        }
+        return ans;
     }
     private int solve(int row, int col1, int col2, int[][] grid, int[][][] container) {
         if (col1 < 0 || col1 >= grid[0].length || col2 < 0 || col2 >= grid[0].length) {
