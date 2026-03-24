7a1eeb854a3c491ee28b19988233a7db1c489119
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2026-03-24 13:44:06 +0530

    Time: 10 ms (84.31%), Space: 100.9 MB (50.49%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.java
+++ 2906-construct-product-matrix/2906-construct-product-matrix.java
@@ -8,27 +8,20 @@ class Solution {
         int m = grid[0].length;
 
         int[][] res = new int[n][m];
-
-        int[][] suffix = new int[n][m];
-
         long mult = 1;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 res[i][j] = (int)mult;
                 mult = (int)(mult * (grid[i][j] % mod)) % mod;
-                System.out.println(i+" "+j+" --> "+res[i][j]);
             }
         }
         mult = 1;
 
         for (int i = n - 1; i >= 0; i--) {
             for (int j = m - 1; j >= 0; j--) {
-                // suffix[i][j] = mult;
                 res[i][j] = (int)(res[i][j] * mult) % mod;
-                // res[i][j] = (res[i][j] * grid[i][j]) % mod;
                 mult = (mult * grid[i][j]) % mod;
-                // res[i][j] = (res[i][j] * suffix[i])
             }
         }
         return res;
