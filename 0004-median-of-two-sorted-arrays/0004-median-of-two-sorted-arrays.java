f46dbeed3a114db02e477c10eed7b54f9009f782
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2026-03-31 11:49:25 +0530

    Time: 1 ms (99.95%), Space: 48.6 MB (90.64%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -2,47 +2,46 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         return solve(nums1, nums2);
     }
-    private double solve(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
+    public double solve(int[] nums1, int[] nums2) {
+        
+        int n1 = nums1.length;
+        int n2 = nums2.length;
 
-        boolean isEvenLength = (n + m) % 2 == 0;
-        int medianIndex = (n + m) / 2;
+        if (n1 > n2) {
+            return solve(nums2, nums1);
+        }
 
-        int idx1 = 0;
-        int idx2 = 0;
+        int low = 0;
+        int high = n1;
 
-        int ans = 0;
+        int left = (n1 + n2 + 1) / 2;
 
-        int val = 0;
-        while (medianIndex >= 0) {
-            if (idx1 >= nums1.length) {
-                val = nums2[idx2];
-                ++idx2;
-            } else if (idx2 >= nums2.length) {
-                val = nums1[idx1];
-                ++idx1;
-            } else if (nums1[idx1] >= nums2[idx2]) {
-                val = nums2[idx2];
-                ++idx2;
-            } else {
-                val = nums1[idx1];
-                ++idx1;
-            }
+        while (low <= high) {
             
-            if (isEvenLength && medianIndex == 1) {
-                System.out.println(val);
-                ans += val;
+            int mid1 = low + (high - low) / 2;       // mid of first array
+            int mid2 = left - mid1;                  // mid of second array
+
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+
+            if (mid1 < n1) r1 = nums1[mid1];
+            if (mid2 < n2) r2 = nums2[mid2];
+
+            if (mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+
+            if (l1 <= r2 && l2 <= r1) {
+                if ((n1 + n2) % 2 == 0) {
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                } else {
+                    return Math.max(l1, l2);
+                }
+            } else if (l1 > r2) {
+                high = mid1 - 1;
+            } else {
+                low = mid1 + 1;
             }
-            if (medianIndex == 0) {
-                System.out.println(val);
-                ans += val;
-            }
-            --medianIndex;
         }
-        if (isEvenLength) {
-            return (ans / 2.0);
-        }
-        return ans * 1.0;
+        return 0;
     }
 }
