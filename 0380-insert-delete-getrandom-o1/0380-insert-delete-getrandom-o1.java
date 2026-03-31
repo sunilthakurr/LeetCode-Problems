03aa71bf39f3a4b5212e013ef24b106591171b47
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2026-03-31 13:30:35 +0530

    Time: 27 ms (55.99%), Space: 100.6 MB (58.07%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,40 +1,48 @@
 class RandomizedSet {
 
-    HashMap<Integer, Integer> valToInd;
+    Random rand;
+    Map<Integer, Integer> map;
     List<Integer> list;
-    int ind = 0;
-    
-    /** Initialize your data structure here. */
+
     public RandomizedSet() {
-        valToInd = new HashMap<>();
+        rand = new Random();
+        map = new HashMap<>();
         list = new ArrayList<>();
     }
     
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     public boolean insert(int val) {
-        if(valToInd.containsKey(val)) return false;
-        list.add(val);
-        valToInd.put(val,list.size()-1);
-        return true;
+        if (!map.containsKey(val)) {
+            list.add(val);
+            map.put(val, list.size() - 1);
+            return true;
+        }
+        return false;
     }
     
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
     public boolean remove(int val) {
-        int ind = valToInd.getOrDefault(val,-1);
-        if(ind == -1) return false;
-        Collections.swap(list,ind,list.size()-1);
-        int swappedWith = list.get(ind);
-        valToInd.put(swappedWith,ind);
-        list.remove(list.size()-1);
-        valToInd.remove(val);
+        if (!map.containsKey(val)) {
+            return false;
+        }
+        int index = map.get(val);
+        int lastElement = list.get(list.size() - 1);
+
+        list.set(index, lastElement);
+        map.put(lastElement, index);
+        list.remove(list.size() - 1);
+        map.remove(val);
         return true;
     }
     
-    /** Get a random element from the set. */
     public int getRandom() {
-        int max = list.size();
-        int min = 0;
-        int ind = (int)(Math.random() * (max - min) + min);
-        return list.get(ind);
+        int randomIndex = rand.nextInt(list.size());
+        return list.get(randomIndex);
     }
 }
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
