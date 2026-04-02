db8ec64bd061886fa956a72e4caebb19336a2d5b
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2026-04-03 01:20:43 +0530

    Time: 0 ms (100%), Space: 43.4 MB (88.91%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,18 +15,14 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root == null){
-            return true;
-        }
+        if (root == null) return true;
         return solve(root.left, root.right);
     }
-    public boolean solve(TreeNode root1, TreeNode root2){
-        if(root1 == null && root2 == null){
-            return true;
-        }
-        if(root1 == null || root2 == null){
-            return false;
-        }
-        return (root1.val == root2.val) && solve(root1.left, root2.right) && solve(root1.right, root2.left);
+    private boolean solve(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root1 == root2) return true;
+        if ((root1 == null && root2 != null) || (root1 != null && root2 == null)) return false;
+
+        if (root1.val != root2.val) return false;
+        return solve(root1.left, root2.right) && solve(root1.right, root2.left);
     }
 }
