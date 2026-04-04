8de909b145ebd1ab2082563a0f34855738d35543
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2026-04-05 00:04:58 +0530

    Time: 31 ms (14.81%), Space: 60.5 MB (81.68%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,45 +1,46 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         
+        List<List<Integer>> adj = new ArrayList<>();
         int V = graph.length;
 
-        boolean[] vis = new boolean[V];
-        boolean[] pathVis = new boolean[V];
-        boolean[] safeStates = new boolean[V];
-
         for (int i = 0; i < V; i++) {
-            if (!vis[i]) {
-                dfs(graph, vis, pathVis, safeStates, i);
-            }
+            adj.add(new ArrayList<>());
         }
 
-        List<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < V; i++) {
-            if (safeStates[i]) {
-                list.add(i);
+        int[] indegree = new int[V];
+        for (int i = 0; i < graph.length; i++) {
+            for (int el: graph[i]) {
+                adj.get(el).add(i);
+                indegree[i]++;
             }
         }
-        return list;
+        return kahnAlgorithm(adj, V, indegree);
     }
 
-    private boolean dfs(int[][] graph, boolean[] vis, boolean[] pathVis, boolean[] safeStates, int node) {
+    private List<Integer> kahnAlgorithm(List<List<Integer>> adj, int V, int[] indegree) {
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> res = new ArrayList<>();
 
-        vis[node] = true;
-        pathVis[node] = true;
-
-        for (int vertex: graph[node]) {
-            if (!vis[vertex]) {
-                if (dfs(graph, vis, pathVis, safeStates, vertex)) {
-                    return true;
-                }
-            } else if (pathVis[vertex]) {
-                return true;
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
             }
         }
 
-        safeStates[node] = true;
-        pathVis[node] = false;
-        return false;
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            res.add(node);
+
+            for (int vertex: adj.get(node)) {
+                indegree[vertex]--;
+                
+                if (indegree[vertex] == 0) {
+                    q.add(vertex);
+                }
+            }
+        }
+        Collections.sort(res);
+        return res;
     }
 }
