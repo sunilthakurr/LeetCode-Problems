eaead231c33078049cb14464005f2416cb55ef1b
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2026-04-04 17:05:55 +0530

    Time: 12 ms (84.69%), Space: 47.8 MB (24.91%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,25 +1,41 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        
-        Map<Integer, Integer> map = new HashMap<>();
+
+        List<Integer>[] bucket = new List[nums.length + 1];
+
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
 
         for (int el: nums) {
-            map.put(el, map.getOrDefault(el, 0) + 1);
+            frequencyMap.put(el, frequencyMap.getOrDefault(el, 0) + 1);
         }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> b[1] - a[1]);
+        for (int key: frequencyMap.keySet()) {
+            int frequency = frequencyMap.get(key);
 
-        for (int key: map.keySet()) {
-            pq.add(new int[]{key, map.get(key)});
-
-            // pq.add(arr);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
         }
 
-        int[] res = new int[k];
+        int[] result = new int[k];
 
-        for (int i = 0; i < k; i++) {
-            res[i] = pq.poll()[0];
+        int idx = 0;
+
+        for (int i = bucket.length - 1; i >= 0 && idx < result.length; i--) {
+            if (bucket[i] != null) {
+                // System.out.println("hii hello");
+                // System.out.println(bucket[i].size());
+                for (int j = 0; j < bucket[i].size(); j++) {
+                    // System.out.println(bucket[i].get(j));
+                    
+                    result[idx] = bucket[i].get(j);
+                    idx++;
+                }
+                // result.addAll(bucket[i]);
+                // k -= bucket[i].size();
+            }
         }
-        return res;
+        return result;
     }
 }
