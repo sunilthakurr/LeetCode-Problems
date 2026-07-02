0728fb53f3c8b815a33a2caa298fa20717603f4f
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2026-07-02 12:55:06 +0530

    Time: 274 ms (5.33%), Space: 47.5 MB (21.72%) - LeetHub

---
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.java
@@ -0,0 +1,56 @@
+class Solution {
+    public boolean findSafeWalk(List<List<Integer>> grid, int health) {
+        int n = grid.size();
+        int m = grid.get(0).size();
+
+        if (grid.get(0).get(0) == 1) {
+            health -= 1;
+        }
+        if (health <= 0) {
+            return false;
+        }
+
+        int[] drow = {-1, 0, +1, 0};
+        int[] dcol = {0, +1, 0, -1};
+        boolean[][] vis = new boolean[n][m];
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        pq.add(new int[]{health, 0, 0});
+
+        while (!pq.isEmpty()) {
+            int[] arr = pq.poll();
+            int remHealth = arr[0];
+            int row = arr[1];
+            int col = arr[2];
+
+            System.out.println(remHealth + " "+row+" "+col);
+
+            if (remHealth <= 0) {
+                return false;
+            }
+
+            if (row == n - 1 && col == m - 1) {
+                return true;
+            }
+
+            if (vis[row][col]) {
+                continue;
+            }
+            vis[row][col] = true;
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol]) {
+                    int newHealth = remHealth;
+                    if (grid.get(nrow).get(ncol) == 1) {
+                        newHealth -= 1;
+                    }
+                    pq.add(new int[] {newHealth, nrow, ncol});
+                }
+            }
+        }
+        return false;
+    }
+}
